d5b0ca7237326b7075488d0bf2c119e0d667dc87
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-09-13 21:34:59 +0200

    updated DB schema to include publication.

--- include/yams_db.hrl
+++ include/yams_db.hrl
@@ -3,16 +3,34 @@
 
 %% Note: cid is chosen as the first field in this record, 
 %% so that it will be the primary key, in the mnesia table.
--record(cid_to_apid, {cid, apid}).
+-record(cid_to_apid, 
+	{
+	  cid, %% Client ID
+	  apid %% Acceptor PID
+	}).
 
 
 %% subcription table will map Client ID with the topics 
 %% subscribed/unsubscribed by the client.
 -record(subscription, 
 	{
-	  cid, %%Client ID
-	  topic, %%Topic
-	  qos, %%QoS
+	  cid, %% Client ID
+	  topic, %% Topic
+	  qos, %% QoS
 	  msgID %% Message ID
+	}).
+
+%% publication table will store MsgID, Topic and Message
+-record(publication, 
+	{
+	  %%yams_msg_ID, %% Unique messag ID assigned by YAMS
+	  cid, %% Client ID
+	  msgID, %% Message ID
+	  topic, %% Topic of the message
+	  dup, %% Duplicate flag
+	  qos, %% Quality of Service
+	  retain, %% Retain Flag
+	  msg, %% Message Published (does not contain first byte and remaining lenght field)
+	  is_retained %% Boolean flag (identifies if the message is the retained one (by YAMS)
 	}).
 
