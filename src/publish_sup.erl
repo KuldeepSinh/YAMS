7cd9c14914a6d55971bd88557c3e0e379110ca27
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-09-13 21:40:12 +0200

    skeleton created to handle publish messages.

---
+++ src/publish_sup.erl
@@ -0,0 +1,85 @@
+%% Copyright 2013 KuldeepSinh Chauhan
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%%%-------------------------------------------------------------------
+%%% @author  KuldeepSinh Chauhan
+%%% @copyright (C) 2013, 
+%%% @doc
+%%%     This module will supervise connect message handlers.
+%%% @end
+%%% Created : 13 Sep 2013 by  KuldeepSinh Chauhan
+%%%-------------------------------------------------------------------
+-module(publish_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, 
+	 start_child/6]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+start_child(APid, ClientID, Dup, QoS, Retain, Msg) ->
+    supervisor:start_child(?SERVER, [APid, ClientID, Dup, QoS, Retain, Msg]).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    RestartStrategy = simple_one_for_one,
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = temporary,
+    Shutdown = 2000,
+    Type = worker,
+
+    Publish = {pusblish, {publish, start_link, []}, Restart, Shutdown, Type, [publish]},
+    {ok, {SupFlags, [Publish]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
